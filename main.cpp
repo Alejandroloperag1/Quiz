1bd9609aaed89a159174adbf01780067fedd0f7b
AlejandroLoperag <alejandro.loperag1@udea.edu.co>
2021-04-09 13:04:51 -0500

    Quiz #1

---
+++ main.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+
+using namespace std; 
+
+int main()
+
+{
+    int h;
+    cout <<"Ingrese un numero entero: ";
+    cin >>h;
+    int h_co*h, suma=0, cifra, potencia, valor_U=0, contar_U=0, sum_fu;;
+    while(h_co=0)
+{
+    valor=h_co%10;
+    if(valor>valor_U
+    {
+        valor_U=valor;
+        contar_U=1;
+    }
+    else if(valor==valor_U)
+    {
+        contar_U++;
+    }
+    potencia=cifra;
+    for (int t=1;t<valor;t++)
+    {
+        sum_fu=0;
+        for(int s=0;s<potencia;s++)
+        {
+            sum_fu+=valor;
+        }
+        potencia=sum_fu;
+        
+    } 
+    suma+=potencia;
+    h_co*h_co/10;
+}
+cout<<"La suma de cada numero elevado por si mismo "<<h<<" es"<<suma<<endl;
+cout<<"El digito mayor encontrado es"<<valor_U<<"que aparece"<<contar_U"veces"endl;
+
+}
